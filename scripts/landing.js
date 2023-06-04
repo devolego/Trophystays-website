de629bb03641743fd1af14797ceb3c19254f11db
Denis Shatalov <dev.olegovich@gmail.com>
2023-06-05 03:21:02 +0400

    70% done with implementing the search bar

--- scripts/landing.js
+++ scripts/landing.js
@@ -3,7 +3,7 @@ const searchDivs = document.querySelectorAll('.search-box-option')
 searchDivs.forEach(searchDiv => {
     searchDiv.setAttribute('tabindex', '0')
 
-    searchDiv.addEventListener('click', event => {
+    searchDiv.addEventListener('click', (event) => {
         const dropdown = searchDiv.querySelector('[data-dropdown]')
 
         if (dropdown) {
@@ -25,12 +25,34 @@ searchDivs.forEach(searchDiv => {
 
     const dropdown = searchDiv.querySelector('[data-dropdown]')
     if (dropdown) {
-        dropdown.addEventListener('click', event => {
+        dropdown.addEventListener('click', (event) => {
             // Stop event propagation to prevent document click handler from firing
             event.stopPropagation()
         })
+
+        // const listItems = dropdown.querySelectorAll('li')
+        // listItems.forEach(listItem => {
+        //     listItem.addEventListener('click', () => {
+        //         const spanElement = searchDiv.querySelector('span')
+        //         spanElement.textContent = listItem.textContent
+        //     })
+        // })
     }
 })
+
+
+
+
+// Add click event listener to document to close dropdowns on outside click
+document.addEventListener('click', () => {
+    searchDivs.forEach(searchDiv => {
+        const dropdown = searchDiv.querySelector('[data-dropdown]')
+        if (dropdown) {
+            dropdown.classList.remove('show')
+        }
+    })
+})
+
 
 // Add click event listener to document to close dropdowns on outside click
 document.addEventListener('click', () => {
@@ -47,6 +69,7 @@ document.addEventListener('click', () => {
 //Location Dropdown
 
 const data = {
+    'All': [],
     'City 1': ['Area 1', 'Area 2', 'Area 3', 'Area 4', 'Area 5'],
     'City 2': ['Area 6', 'Area 7', 'Area 8', 'Area 9', 'Area 10'],
     'City 3': ['Area 11', 'Area 12', 'Area 13', 'Area 14', 'Area 15'],
@@ -57,27 +80,63 @@ const data = {
 const dropdown = document.querySelector('.dropdown-sb')
 
 const cities = Object.keys(data);
-for (let i = 0; i < cities.length; i += 2) {
+for (let i = 0; i < cities.length;) {
     const column = document.createElement('div')
     column.className = 'column'
-    for (let j = i; j < i + 2 && j < cities.length; j++) {
-        const city = cities[j];
+
+    // decide the number of cities per column
+    let numCities = (i === 0) ? 3 : 2;
+
+    for (let j = 0; j < numCities && i < cities.length; j++, i++) {
+        const city = cities[i];
 
         const title = document.createElement('h4')
         title.innerText = city
+        // Assign the "all-city" class if city is "All"
+        if (city === 'All') {
+            title.className = 'all-city';
+        }
         column.appendChild(title)
 
-        const ul = document.createElement('ul')
-        data[city].forEach(area => {
-            const li = document.createElement('li')
-            li.innerText = area
-            ul.appendChild(li)
-        })
-
-        column.appendChild(ul)
+        // Skip adding ul if city is "All"
+        if (city !== 'All') {
+            const ul = document.createElement('ul')
+            data[city].forEach(area => {
+                const li = document.createElement('li')
+                li.innerText = area
+                ul.appendChild(li)
+            })
+            column.appendChild(ul)
+        }
     }
 
     dropdown.appendChild(column)
 }
 
+
+
+
+
+// First, select all the clickable dropdown options
+let citiesOptions = document.querySelectorAll('.dropdown-sb h4');
+let serviceOptions = document.querySelectorAll('.dropdown-sb li');
+let termOptions = document.querySelectorAll('.dropdown-sp li');
+let bedroomOptions = document.querySelectorAll('#bedrooms-dropdown li');
+
+// Merge the arrays into one
+let options = [...citiesOptions, ...serviceOptions, ...termOptions, ...bedroomOptions];
+
+console.log(options);
+
+// Then, add a click event listener to each option
+options.forEach(option => {
+    option.addEventListener('click', () => {
+        // When an option is clicked, get its text content
+        let selectedOption = option.textContent;
+
+        // Then, find the span element within the parent search-box-option and replace its text content with the selected option
+        let spanElement = option.closest('.search-box-option').querySelector('span');
+        spanElement.textContent = selectedOption;
+    });
+});
 
