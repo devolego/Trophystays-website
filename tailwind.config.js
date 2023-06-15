c46acc035f8654cbb44416bbb68f61487b65863f
Shivam <shivampurohit266@gmail.com>
2023-06-15 12:53:11 +0530

    creating style guide

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,29 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./**/*.{html,js,ejs}"],
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        white: "#ffffff",
+        black: "#292021",
+        blackLight: "#333333",
+        primary: "#886750",
+        secondary: "#CAB08F",
+        darkGray: "#939393",
+        lightBrown: "#F5EADB",
+        greyishBrown: "#E1D9CE",
+        offWhite: "#FAFAFA",
+      },
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+    },
   },
   plugins: [],
-}
+};
---
+++ utils.tsx
@@ -0,0 +1,6 @@
+import { Josefin_Sans } from "next/font/google";
+
+// const inter = Inter({ subsets: ["latin"] });
+export const josefin = Josefin_Sans({
+  subsets: ["latin"],
+});
