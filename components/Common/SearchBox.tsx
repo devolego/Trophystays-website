77d5ba74eef3909a9f474401b1191133175fbc23
Shivam <shivampurohit266@gmail.com>
2023-06-21 16:52:12 +0530

    modified the searchBox

--- components/Common/SearchBox.tsx
+++ components/Common/SearchBox.tsx
@@ -9,10 +9,13 @@ import bedroomIcon from "../../images/bedroom.png";
 import arrivalIcon from "../../images/arrival.png";
 import Link from "next/link";
 import {
+  arrivalItems,
+  bedroomsItems,
   departureItems,
   locationFilterItems,
   termMenuItems,
 } from "../../utils/utilsItems";
+import SearchBoxDropdown from "./SearchBoxDropdown";
 
 const SearchBox = ({ searchClasses }) => {
   const [locationItems, setLocationItems] = useState(false);
@@ -37,28 +40,25 @@ const SearchBox = ({ searchClasses }) => {
   };
   return (
     <div
-      className={`bg-white p-[30px] rounded-xl shadow-md max-w-[1190px] w-full m-auto relative z-1 ${searchClasses} max-h-[166px] h-full`}
+      className={`bg-white p-[30px] rounded-xl shadow-md max-w-[1190px] w-full m-auto relative z-1 ${searchClasses} xl:max-h-[166px] h-full max-xl:h-max`}
     >
       <h3 className={`text-2xl ${josefin.className} mb-4`}>
         Search for your most needed hotels.
       </h3>
-      <div className="flex justify-between gap-4 relative">
-        <div className="relative w-[185px]" onClick={handleLocationItems}>
-          <div className="flex p-4 border rounded-lg border-greyishBrown w-max relative cursor-pointer w-[185px]">
-            <Image src={locationIcon} alt="" className="mr-3 object-contain" />
-            <p className="text-base text-darkGrey after:bg-down-arrow after:absolute after:top-[50%] after:right-[22px] after:-translate-y-2/4 after:w-3 after:bg-no-repeat after:h-2 after:bg-center">
-              Filter
-            </p>
-          </div>
-
+      <div className="flex justify-between gap-4 relative max-xl:grid max-xl:grid-cols-3 max-md:grid-cols-1">
+        <div
+          className="relative xl:w-[185px] max-xl:w-full"
+          onClick={handleLocationItems}
+        >
+          <SearchBoxDropdown imageSrc={locationIcon} seachHeading="Filter" />
           {locationItems && (
             <div>
-              <div className="p-4 rounded-xl grid grid-cols-2 w-[587px] bg-white mt-3">
+              <div className="p-4 rounded-xl grid grid-cols-2 max-w-[587px] bg-white mt-3 max-md:grid-cols-1">
                 {locationFilterItems.map((items) => {
                   return (
                     <div
                       key={items.id}
-                      className="odd:border-r odd:border-[#E1D9CE] odd:mr-[30px]"
+                      className="odd:border-r odd:border-[#E1D9CE] odd:mr-[30px] max-md:border-none"
                     >
                       <h2 className="text-base font-semibold mt-[30px]">
                         {items.heading}
@@ -79,17 +79,14 @@ const SearchBox = ({ searchClasses }) => {
         </div>
 
         {/* 2nd box */}
-        <div className="relative w-[185px]" onClick={handelTermItems}>
-          <div className="flex p-4 border rounded-lg border-greyishBrown w-max relative cursor-pointer w-[185px]">
-            <Image src={termIcon} alt="" className="mr-3 object-contain" />
-            <p className="text-base text-darkGrey after:bg-down-arrow after:absolute after:top-[50%] after:right-[22px] after:-translate-y-2/4 after:w-3 after:bg-no-repeat after:h-2 after:bg-center">
-              Term
-            </p>
-          </div>
-
+        <div
+          className="relative xl:w-[185px] max-xl:w-full"
+          onClick={handelTermItems}
+        >
+          <SearchBoxDropdown imageSrc={termIcon} seachHeading="Term" />
           {termItem && (
             <div>
-              <div className="p-4 rounded-xl grid grid-cols-2 w-[587px] bg-white mt-3">
+              <div className="p-4 rounded-xl grid grid-cols-2 bg-white mt-3">
                 {termMenuItems.map((items, index) => {
                   return (
                     <div key={index} className="">
@@ -103,33 +100,18 @@ const SearchBox = ({ searchClasses }) => {
         </div>
 
         {/* 3rd box */}
-        <div className="relative w-[185px]" onClick={handleBedRoomItem}>
-          <div className="flex p-4 border rounded-lg border-greyishBrown w-max relative cursor-pointer w-[185px]">
-            <Image src={bedroomIcon} alt="" className="mr-3 object-contain" />
-            <p className="text-base text-darkGrey after:bg-down-arrow after:absolute after:top-[50%] after:right-[22px] after:-translate-y-2/4 after:w-3 after:bg-no-repeat after:h-2 after:bg-center">
-              Bedrooms
-            </p>
-          </div>
-
+        <div
+          className="relative xl:w-[185px] max-xl:w-full"
+          onClick={handleBedRoomItem}
+        >
+          <SearchBoxDropdown imageSrc={bedroomIcon} seachHeading="Bedrooms" />
           {bedRoomItem && (
             <div>
-              <div className="p-4 rounded-xl grid grid-cols-2 w-[587px] bg-white mt-3">
-                {locationFilterItems.map((items) => {
+              <div className="p-4 rounded-xl grid grid-cols-2 bg-white mt-3">
+                {bedroomsItems.map((items, index) => {
                   return (
-                    <div
-                      key={items.id}
-                      className="odd:border-r odd:border-[#E1D9CE] odd:mr-[30px]"
-                    >
-                      <h2 className="text-base font-semibold mt-[30px]">
-                        {items.heading}
-                      </h2>
-                      {items.menuItem.map((value, index) => {
-                        return (
-                          <div key={index} className="font-extralight p-">
-                            {value}
-                          </div>
-                        );
-                      })}
+                    <div key={index} className="text-base">
+                      {items}
                     </div>
                   );
                 })}
@@ -139,33 +121,18 @@ const SearchBox = ({ searchClasses }) => {
         </div>
 
         {/* 4th box */}
-        <div className="relative w-[185px]" onClick={handleArrivalMenuItem}>
-          <div className="flex p-4 border rounded-lg border-greyishBrown w-max relative cursor-pointer w-[185px]">
-            <Image src={arrivalIcon} alt="" className="mr-3 object-contain" />
-            <p className="text-base text-darkGrey after:bg-down-arrow after:absolute after:top-[50%] after:right-[22px] after:-translate-y-2/4 after:w-3 after:bg-no-repeat after:h-2 after:bg-center">
-              Arrival
-            </p>
-          </div>
-
+        <div
+          className="relative xl:w-[185px] max-xl:w-full"
+          onClick={handleArrivalMenuItem}
+        >
+          <SearchBoxDropdown imageSrc={arrivalIcon} seachHeading="Arrival" />
           {arrivalMenuItem && (
             <div>
-              <div className="p-4 rounded-xl grid grid-cols-2 w-[587px] bg-white mt-3">
-                {locationFilterItems.map((items) => {
+              <div className="p-4 rounded-xl grid grid-cols-2 bg-white mt-3">
+                {arrivalItems.map((items, index) => {
                   return (
-                    <div
-                      key={items.id}
-                      className="odd:border-r odd:border-[#E1D9CE] odd:mr-[30px]"
-                    >
-                      <h2 className="text-base font-semibold mt-[30px]">
-                        {items.heading}
-                      </h2>
-                      {items.menuItem.map((value, index) => {
-                        return (
-                          <div key={index} className="font-extralight p-">
-                            {value}
-                          </div>
-                        );
-                      })}
+                    <div key={index} className="text-base">
+                      {items}
                     </div>
                   );
                 })}
@@ -175,17 +142,14 @@ const SearchBox = ({ searchClasses }) => {
         </div>
 
         {/* 5th box  */}
-        <div className="relative w-[185px]" onClick={handleDepartureMenuItem}>
-          <div className="flex p-4 border rounded-lg border-greyishBrown w-max relative cursor-pointer w-[185px]">
-            <Image src={arrivalIcon} alt="" className="mr-3 object-contain" />
-            <p className="text-base text-darkGrey after:bg-down-arrow after:absolute after:top-[50%] after:right-[22px] after:-translate-y-2/4 after:w-3 after:bg-no-repeat after:h-2 after:bg-center">
-              Departure
-            </p>
-          </div>
-
+        <div
+          className="relative xl:w-[185px] max-xl:w-full"
+          onClick={handleDepartureMenuItem}
+        >
+          <SearchBoxDropdown imageSrc={arrivalIcon} seachHeading="Departure" />
           {departureMenuItem && (
             <div>
-              <div className="p-4 rounded-xl grid grid-cols-2 w-[587px] bg-white mt-3">
+              <div className="p-4 rounded-xl grid grid-cols-2 bg-white mt-3">
                 {departureItems.map((items, index) => {
                   return (
                     <div key={index} className="">
@@ -197,10 +161,11 @@ const SearchBox = ({ searchClasses }) => {
             </div>
           )}
         </div>
+
         <Button
           ButtonText={"Search"}
           ButtonClasses={
-            "w-max flex items-center text-white justify-center max-h-[58px]"
+            "w-max flex items-center text-white justify-center max-h-[58px] max-xl:w-full max-md:py-3"
           }
         ></Button>
       </div>
