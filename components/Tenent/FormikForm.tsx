6e49fcc80749c491e098ef1f52fe1489afca683c
Rishabh Bula <rishabhbula1712@gmail.com>
2023-06-28 20:28:48 +0530

    Worked on booking property page in tenent pages section

--- components/Tenent/FormikForm.tsx
+++ components/Tenent/FormikForm.tsx
@@ -38,65 +38,105 @@ const FormikForm = ({ isSubmitting }: any) => {
       validationSchema={validationSchema}
       onSubmit={onSubmit}
     >
-      <Form className="flex flex-wrap justify-between gap-4">
-        <div className="form-control p-4 border border-solid border-greyishBrown rounded-[8px] h-[60px]">
-          <Field
-            type="text"
-            id="firstname"
-            name="firstname"
-            placeholder="First name"
-          />
-          <ErrorMessage className="error" name="firstname" component="div" />
-        </div>
+      <Form>
+        <div className="flex gap-8">
+          <div className="w-1/2">
+            <div className="mb-5 text-xl font-medium text-black">
+              Guest Details
+            </div>
+            <div className="flex flex-col gap-4">
+              <div className="form-control p-4 border border-solid border-greyishBrown rounded-[8px] h-[60px]">
+                <Field
+                  type="text"
+                  id="firstname"
+                  name="firstname"
+                  placeholder="First name"
+                />
+                <ErrorMessage
+                  className="error"
+                  name="firstname"
+                  component="div"
+                />
+              </div>
 
-        <div className="form-control p-4 border border-solid border-greyishBrown rounded-[8px] h-[60px]">
-          <Field
-            type="text"
-            id="companyname"
-            name="companyname"
-            placeholder="Company name"
-          />
-          <ErrorMessage className="error" name="companyname" component="div" />
-        </div>
-
-        <div className="form-control p-4 border border-solid border-greyishBrown rounded-[8px] h-[60px]">
-          <Field
-            type="text"
-            id="lastname"
-            name="lastname"
-            placeholder="Last name"
-          />
-          <ErrorMessage className="error" name="lastname" component="div" />
-        </div>
-
-        <div className="form-control p-4 border border-solid border-greyishBrown rounded-[8px] h-[60px]">
-          <Field
-            type="text"
-            id="address"
-            name="address"
-            placeholder="Address"
-          />
-          <ErrorMessage className="error" name="address" component="div" />
-        </div>
-
-        <div className="form-control p-4 border border-solid border-greyishBrown rounded-[8px] h-[60px]">
-          <Field type="email" id="email" name="email" placeholder="Email" />
-          <ErrorMessage className="error" name="email" component="div" />
-        </div>
-
-        <div className="form-control p-4 border border-solid border-greyishBrown rounded-[8px] h-[60px]">
-          <Field
-            type="text"
-            id="country"
-            name="country"
-            placeholder="country"
-          />
-          <ErrorMessage className="error" name="country" component="div" />
-        </div>
-
-        <div className="form-control p-4 border border-solid border-greyishBrown rounded-[8px] h-[60px]">
-          <Field type="tel" id="phone" name="phone" placeholder="Phone no." />
-          <ErrorMessage className="error" name="phone" component="div" />
+              <div className="form-control p-4 border border-solid border-greyishBrown rounded-[8px] h-[60px]">
+                <Field
+                  type="text"
+                  id="lastname"
+                  className="flex flex-col gap-4e"
+                />
+                <ErrorMessage
+                  className="error"
+                  name="lastname"
+                  component="div"
+                />
+              </div>
+              <div className="form-control p-4 border border-solid border-greyishBrown rounded-[8px] h-[60px]">
+                <Field
+                  type="email"
+                  id="email"
+                  name="email"
+                  placeholder="Email"
+                />
+                <ErrorMessage className="error" name="email" component="div" />
+              </div>
+              <div className="form-control p-4 border border-solid border-greyishBrown rounded-[8px] h-[60px]">
+                <Field
+                  type="tel"
+                  id="phone"
+                  name="phone"
+                  placeholder="Phone no."
+                />
+                <ErrorMessage className="error" name="phone" component="div" />
+              </div>
+            </div>
+          </div>
+          <div className="w-1/2">
+            <div className="mb-5 text-xl font-medium text-black">
+              Guest Details
+            </div>
+            <div className="flex flex-col gap-4">
+              <div className="form-control p-4 border border-solid border-greyishBrown rounded-[8px] h-[60px]">
+                <Field
+                  type="text"
+                  id="companyname"
+                  name="companyname"
+                  placeholder="Company name"
+                />
+                <ErrorMessage
+                  className="error"
+                  name="companyname"
+                  component="div"
+                />
+              </div>
+              <div className="form-control p-4 border border-solid border-greyishBrown rounded-[8px] h-[60px]">
+                <Field
+                  type="text"
+                  id="address"
+                  name="address"
+                  placeholder="Address"
+                />
+                <ErrorMessage
+                  className="error"
+                  name="address"
+                  component="div"
+                />
+              </div>
+              <div className="form-control p-4 border border-solid border-greyishBrown rounded-[8px] h-[60px]">
+                <Field
+                  type="text"
+                  id="country"
+                  name="country"
+                  placeholder="country"
+                />
+                <ErrorMessage
+                  className="error"
+                  name="country"
+                  component="div"
+                />
+              </div>
+            </div>
+          </div>
         </div>
 
         {/* <button type="reset">Reset data</button>
@@ -109,69 +149,3 @@ const FormikForm = ({ isSubmitting }: any) => {
 };
 
 export default FormikForm;
-
-// return (
-//     <div>
-//       <Formik
-//         initialValues={{
-//           firstname: "",
-//           lastname: "",
-//           email: "",
-//           phone: "",
-//           companyname: "",
-//           address: "",
-//           country: "",
-//         }}
-//         validate={(values) => {
-//           const errors = {};
-//             if (!values.firstname) {
-//               errors.firstname = 'Required';
-//             } else if (!values.lastname) {
-//                 errors.firstname = 'Required';
-//             } else if (!values.email) {
-//               errors.email = 'Required';
-//             } else if (
-//               !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
-//             ) {
-//               errors.email = 'Invalid email address';
-//             }
-//           return errors;
-//         }}
-//         onSubmit={(values, { setSubmitting }) => {
-//           setTimeout(() => {
-//             alert(JSON.stringify(values, null, 2));
-//             setSubmitting(false);
-//           }, 400);
-//         }}
-//       >
-//         {({ isSubmitting }) => (
-//           <Form>
-//             <Field type="text" name="firstname" />
-//             <ErrorMessage name="firstname" component="div" />
-
-//             <Field type="text" name="lastname" />
-//             <ErrorMessage name="lastname" component="div" />
-
-//             <Field type="email" name="email" />
-//             <ErrorMessage name="email" component="div" />
-
-//             <Field type="tel" name="phone" />
-//             <ErrorMessage name="email" component="div" />
-
-//             <Field type="text" name="companyname" />
-//             <ErrorMessage name="companyname" component="div" />
-
-//             <Field type="text" name="address" />
-//             <ErrorMessage name="address" component="div" />
-
-//             <Field type="text" name="country" />
-//             <ErrorMessage name="country" component="div" />
-
-//             <button type="submit" disabled={isSubmitting}>
-//               Submit
-//             </button>
-//           </Form>
-//         )}
-//       </Formik>
-//     </div>
-//   );
