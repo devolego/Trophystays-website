7c520a848773d6eed8f466e7676a5f79adc0a2bb
Shivam <shivampurohit266@gmail.com>
2023-06-29 12:42:25 +0530

    updated the constact page for responsive

--- components/Contact/ContactForm.tsx
+++ components/Contact/ContactForm.tsx
@@ -1,6 +1,8 @@
 "use client";
 import { Formik, Field, Form } from "formik";
+import Image from "next/image";
 import React from "react";
+import arrowDropdown from "../../images/arrow-down-solid.png";
 
 const ContactForm = () => {
   return (
@@ -11,6 +13,7 @@ const ContactForm = () => {
           phoneNumber: "",
           email: "",
           subject: "",
+          messageText: "",
         }}
         onSubmit={async (values) => {
           await new Promise((r) => setTimeout(r, 500));
@@ -19,25 +22,34 @@ const ContactForm = () => {
       >
         <Form className="flex flex-wrap justify-between gap-6 mb-[100px]">
           <Field
-            className="p-4 border rounded-lg placeholder:font-semibold border-greyishBrown basis-[48%]"
+            className="p-4 border rounded-lg placeholder:font-medium border-greyishBrown max-md:w-full md:basis-[48%]"
             id="firstName"
             name="firstName"
             placeholder="Name"
           />
 
-          <Field
-            className="p-4 border rounded-lg placeholder:font-semibold border-greyishBrown basis-[48%] bg-transparent"
-            as="select"
-            name="subject"
-          >
-            <option value="ChooseSubject">Choose Subject</option>
-            <option value="red">Red</option>
-            <option value="green">Green</option>
-            <option value="blue">Blue</option>
-          </Field>
+          <div className="relative max-md:w-full md:basis-[48%] ">
+            <Field
+              className="w-full p-4 bg-transparent border rounded-lg appearance-none placeholder:font-medium border-greyishBrown "
+              as="select"
+              name="subject"
+            >
+              <option value="ChooseSubject" className="text-darkGrey">
+                Choose Subject
+              </option>
+              <option value="sub1">Subject</option>
+              <option value="sub2">Subject</option>
+              <option value="sub3">Subject</option>
+            </Field>
+            <Image
+              src={arrowDropdown}
+              alt=""
+              className="absolute object-contain top-[50%] -translate-y-[50%] right-5"
+            />
+          </div>
 
           <Field
-            className="p-4 border rounded-lg placeholder:font-semibold border-greyishBrown basis-[48%] placeholder:text-darkGrey"
+            className="p-4 border rounded-lg placeholder:font-medium border-greyishBrown max-md:w-full md:basis-[48%] placeholder:text-darkGrey"
             id="email"
             name="email"
             placeholder="Email"
@@ -45,14 +57,21 @@ const ContactForm = () => {
           />
 
           <Field
-            className="p-4 border rounded-lg placeholder:font-semibold border-greyishBrown basis-[48%]"
+            className="p-4 border rounded-lg placeholder:font-medium border-greyishBrown max-md:w-full md:basis-[48%]"
             id="phoneNumber"
             name="phoneNumber"
             placeholder="Phone"
           />
+          <Field
+            className="w-full p-4 border rounded-lg placeholder:font-medium border-greyishBrown"
+            name="messageText"
+            component="textarea"
+            rows="2"
+            placeholder="Message"
+          />
 
           <button
-            className="py-3 text-white rounded-lg px-9 bg-primary"
+            className="ml-[auto] py-3 text-white rounded-lg px-9 bg-primary"
             type="submit"
           >
             Submit
